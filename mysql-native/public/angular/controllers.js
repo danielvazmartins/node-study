3311f9365aa8980478af90057f2032b109ffcfd6
Daniel Martins <daniel.martins@viewit.com.br>
2017-08-23 17:59:04 -0300

    Daniel - criando APIs

--- mysql-native/public/angular/controllers.js
+++ mysql-native/public/angular/controllers.js
@@ -1,7 +1,6 @@
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 	
-	$scope.users = [];
-	$scope.test = 'test';
+	$scope.users = [];	
 
 	var init = function() {
 		getUsers();
@@ -13,7 +12,48 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 		.then(function successCallback(response) {			
 			$scope.users = response.data;
 		});
-	}	
+	}
+
+	// Adicionar usuário
+	$scope.addUser = function() {
+		var _data = {
+			name: $scope.name,
+			email: $scope.email
+		}
+		$http.put('/api/users', _data)
+		.then(function successCallback(response) {
+			console.log(response);
+			if ( response.data != "" ) {
+				getUsers();
+			}
+		});
+	}
+
+	// Seleciona usuário para edição
+	$scope.editUser = function(user) {
+		$scope.name = user.usr_name;
+		$scope.email = user.usr_email;
+	}
+
+	// Salva usuário alterado
+	$scope.updUser = function(user) {
+		var _data = {
+			id: user.usr_id,
+			name: user.usr_name,
+			email: user.usr_email
+		}
+		$http.post('/api/users', _data)
+		.then(function successCallback(response) {
+			if ( response.data != "" ) {
+				getUsers();
+			}
+		});
+	}
+
+	// Remover usuário
+	$scope.delUser = function() {
+		console.log('delUser');
+	}
 
 	init();
 }]);
--- mysql-native/routes/api/users.js
+++ mysql-native/routes/api/users.js
@@ -6,11 +6,35 @@ var config = require('../../config/config.js');
 // Cria a conexão com o banco de dados
 var con = mysql.createConnection(config.mysql);
 
-router.get('/', function (request, response) {
-    con.query('SELECT * FROM users', function (error, data) {
+// Retorna a lista de usuários
+router.get('/', function(request, response) {
+    con.query('SELECT * FROM users', function(error, data) {
+		response.json(data);
+    });
+});
+
+// Insere um usuário novo
+router.put('/', function(request, response) {
+	var usr_name = request.body.name;
+	var usr_email = request.body.email;
+	con.query('INSERT INTO users (usr_name, usr_email) VALUES(?,?)', [usr_name, usr_email], function(error, data) {
         console.log(error, data);
 		response.json(data);
     });
 });
+
+// Atualiza um usuário
+router.post('/', function(request, response) {	
+	var usr_id = request.body.usr_id;
+	var params = {
+		usr_name: request.body.usr_name,
+		usr_email: request.body.usr_email
+	}
+	con.query('UPDATE users SET ? WHERE usr_id = ?', [params, usr_id], function(error, data) {
+		console.log(error, data);
+		response.json(data);
+	});
+});
+
 
 module.exports = router;
