6cf91ad268f18e6e4fac65b82bc7584dd99a3994
Daniel Vaz Villalobos Martins <danielvazmartins@gmail.com>
2021-03-31 17:18:28 -0300

    removendo informacoes sensiveis

--- bot-telegram/app.js
+++ bot-telegram/app.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '728304414:AAFTYz8JJHgZHyTRsnGxcD4Rhj5oVC-gk3k';
+const token = 'xxxxxxxxx';
 
 // Create a bot that uses 'polling' to fetch new updates
 // Utilizar o polling se for ouvir requisições
